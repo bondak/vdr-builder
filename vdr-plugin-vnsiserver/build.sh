a2837b2e700fd95a4e32a74e0c6e50878d8b7e6c
Andrey Pavlenko <andrey.a.pavlenko@gmail.com>
2013-03-12 15:30:35 +0400

    Share allowed_hosts.conf among plugins

--- vdr-plugin-vnsiserver/build.sh
+++ vdr-plugin-vnsiserver/build.sh
@@ -10,9 +10,24 @@ DIR="$(cd "$(dirname "$0")" && pwd)"
 . "$DIR/../commons.sh"
 
 version() {
-    local delta='406'
+    local delta='409'
     local version=$(git --git-dir="$SRC_DIR/.git" show "$REV:$RELATIVE_SRC_DIR/vnsi.h" | awk '/static const char \*VERSION *=/ { print $6 }' | tr -d '[";]')
     _pkg_version "$version" "$delta"
+}
+
+_deb_dir() {
+    local deb_dir="$BUILD_DIR/debian"
+    
+    if [ ! -d "$deb_dir" ]
+    then
+        local makefile="$1/Makefile"
+        local name="$(awk '/PLUGIN *= */ {print $3}' "$makefile")"
+        cp -r "$DIR/debian" "$deb_dir"
+        sed -i "s/vnsiserver/$name/g" "$deb_dir/links"
+        sed -i "s/#VDR_VERSION#/$(_vdr_version)/g" "$deb_dir/control"
+    fi
+
+    echo "$deb_dir"
 }
 
 _main $@
