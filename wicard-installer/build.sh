f79445c014ed3dbce2ae86bd178e437c7ede08f3
Andrey Pavlenko <andrey.a.pavlenko@gmail.com>
2013-05-03 17:09:45 +0400

    [wicard-installer] Added alt_url to the postinst script with reference to WICARD/previous folder.

--- wicard-installer/build.sh
+++ wicard-installer/build.sh
@@ -7,11 +7,6 @@ WICARD_URL='http://sat-forum.info/download/index.php?directory=WICARD'
 DIR="$(cd "$(dirname "$0")" && pwd)"
 . "$DIR/../commons.sh"
 
-wicard_dir_url() {
-    local dir="$(curl "$WICARD_URL" 2>/dev/null | grep -Eo "directory=WICARD/wicardd-[a-zA-Z_0-9\.]+")"
-    echo "$WICARD_URL" | sed "s;directory=WICARD;$dir;"
-}
-
 version() {
 	local bs_ci_count=$(git --git-dir="$DIR/../.git" log --format='%H' -- "$PKG_NAME" | wc -l)
     local version="$(curl "$WICARD_URL" 2>/dev/null | grep -Eo "directory=WICARD/wicardd-[a-zA-Z_0-9\.]+" | awk -F '[-_]' '{print $2}')"
@@ -29,8 +24,13 @@ _deb_dir() {
     
     if [ ! -d "$deb_dir" ]
     then
+    	local dir="$(curl "$WICARD_URL" 2>/dev/null | grep -Eo "directory=WICARD/wicardd-[a-zA-Z_0-9\.]+")"
+    	local wicard_dir_url="$(echo "$WICARD_URL" | sed "s;directory=WICARD;$dir;")"
+    	local alt_wicard_dir_url="$(echo "$wicard_dir_url" | sed "s;directory=WICARD;directory=WICARD/previous;")"
+    	
         cp -r "$DIR/debian" "$deb_dir"
-        sed -i "s;@WICARD_URL_PATTERN@;$(wicard_dir_url)\&filename=@FILENAME@\&action=downloadfile;g" "$deb_dir/rules"
+        sed -i "s;@WICARD_URL_PATTERN@;$wicard_dir_url\&filename=@FILENAME@\&action=downloadfile;g" "$deb_dir/rules"
+        sed -i "s;@ALT_WICARD_URL_PATTERN@;$alt_wicard_dir_url\&filename=@FILENAME@\&action=downloadfile;g" "$deb_dir/rules"
     fi
     
     echo "$deb_dir"
