3942eb89d27646172df6b76c3c62399af172413c
Andrey Pavlenko <andrey.a.pavlenko@gmail.com>
2014-01-30 01:03:03 +0400

    Revert "[vdr-plugin-dvbapi] Rolled back to the 1.0.6 version because there are too many problems with 2.0.0"
    This reverts commit 0adb3b108e4fbef2edaf1b4e0b22dd08dbc05694.

--- vdr-plugin-dvbapi/build.sh
+++ vdr-plugin-dvbapi/build.sh
@@ -3,15 +3,14 @@ set -e
 
 PKG_NAME="$(cd "$(dirname "$0")"; basename "$PWD")"
 SRC_URL='https://github.com/manio/vdr-plugin-dvbapi.git'
-REV='9b4ddce97beb0bbccd733d82510092dd7a6b9bb2'
 
 DIR="$(cd "$(dirname "$0")" && pwd)"
 . "$DIR/../commons.sh"
 
 version() {
-    local delta='55'
+    local delta='8'
     local version=$(git --git-dir="$SRC_DIR/.git" show $REV:DVBAPI.h | awk '/static const char \*VERSION *=/ {print $6}' | tr -d '[";]')
-    _pkg_version "2.0.0" "$delta"
+    _pkg_version "$version" "$delta"
 }
 
 _main $@
