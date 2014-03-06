b234f16b63406acf4a20bb36de85ac37d980b824
Andrey Pavlenko <andrey.a.pavlenko@gmail.com>
2014-03-06 15:48:59 +0400

    [aceproxy] Removed version dependency on vdr.

--- aceproxy/build.sh
+++ aceproxy/build.sh
@@ -8,8 +8,12 @@ DIR="$(cd "$(dirname "$0")" && pwd)"
 . "$DIR/../commons.sh"
 
 version() {
+    local delta='235'
+    local ci_count="$(git --git-dir="$SRC_DIR/.git" log --format='%H' $REV -- "$RELATIVE_SRC_DIR" | wc -l)"
+    local sha="$(git --git-dir="$SRC_DIR/.git" log --format='%h' -n1 $REV -- "$RELATIVE_SRC_DIR")"
+    local bs_ci_count="$(git --git-dir="$DIR/../.git" log --format='%H' -- "$PKG_NAME" | wc -l)"
     local version=$(git --git-dir="$SRC_DIR/.git" describe | awk -F '[v-]' '{print $2}')
-    _pkg_version "$version"
+    echo "${version}-$(($ci_count + $bs_ci_count + $delta))~${sha}"
 }
 
 _main $@
